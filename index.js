bb1d72d669addedf67023519d28d4fc6a7eba363
a-r-t <alexthimineur@gmail.com>
2018-12-07 21:46:59 -0500

    major navbar fixes, current page is highlighted, various formatting fixes

--- index.js
+++ index.js
@@ -37,3 +37,16 @@ function sendMessage() {
 		$('#messageToUser').css("color","green");
 	}
 }
+
+function changeLoginIconColor(status) {
+	if (status === 'hover') {
+		$('#login-icon').css("color","black");
+	}
+	else if (status === 'click') {
+		$('#login-icon').css("color","black");
+	}
+	else if (status === 'leave') {
+		$('#login-icon').css("color","#A6B556");
+	}
+
+}
