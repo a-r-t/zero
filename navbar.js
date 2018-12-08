7db6a15ec358d5dd859671c5141ce13f59371209
a-r-t <alexthimineur@gmail.com>
2018-12-07 23:05:58 -0500

    added comment to navbar.js, added merch to navbar in several other pages not being worked on right now

--- navbar.js
+++ navbar.js
@@ -1,3 +1,5 @@
+// function to change login icon color based on status arg (hover, click, or leave)
+// necessary because the glyphicon needs to change color when section of navbar is highlighted, which it cannot do on its own with an event
 function changeLoginIconColor(status) {
 	if (status === 'hover') {
 		$('#login-icon').css("color","black");
